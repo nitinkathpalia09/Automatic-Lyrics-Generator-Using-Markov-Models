05f2ec59e0a9bfc469479589d3dc029974588b22
nitinkathpalia <nn.kathpalia@gmail.com>
2018-10-26 16:58:26 +0530

    commit

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import random
-import markovify
-import ast
-from rhyme import rhyme_finder
-from sylco import sylco
-
+import markovify #uses markov models to generate new sentences
+import ast  
+from rhyme import rhyme_finder #uses cmudict to find rhyming words based on phonetics
+from sylco import sylco  #to find number of syllables 
+""" Preprocessing_Stage
+Includes cleaning text files, 
+removing punctuations 
+and demarking lines clearly   
+"""
 # Open and model lyrics
 with open('lavigne_verse.txt') as f:
     verse_text = f.read()
@@ -12,8 +16,8 @@ with open('lavigne_chorus.txt') as f:
 with open('lyrics_tokenize.txt') as f:
     tokenized_text = f.read()
 
-verse_model = markovify.NewlineText(verse_text, state_size=2)
-chorus_model = markovify.NewlineText(chorus_text, state_size=2)
+verse_model = markovify.NewlineText(verse_text, state_size=2) #to demark new lines in verse raw text 
+chorus_model = markovify.NewlineText(chorus_text, state_size=2) #to demark new lines in chorus raw text 
 
 # Evaluate tokenized_text as a list
 tokenized_text = ast.literal_eval(tokenized_text)
@@ -40,7 +44,7 @@ def match_rhyme(stem, verse_model):
     # If rhymes exist generate lines
     for n in range(100):
         while True:
-            rhyme_line = verse_model.make_sentence()
+            rhyme_line = verse_model.make_sentence() #make random sentence with proper syntax and semantics from verse model
 
             if rhyme_line is not None:
 
@@ -132,7 +136,11 @@ def make_chorus(chorus_model):
 def make_song(chorus_model, verse_model):
 
     song_chorus = make_chorus(chorus_model)
-
+"""can change according to the structure 
+needed for the song
+(this one has a verse followed by a chorus 
+followed by 2 verses and then a the same chorus twice)
+"""
     song = make_verse(verse_model) + '\n' + song_chorus + '\n' \
             + make_verse(verse_model) + '\n' + make_verse(verse_model) + '\n'\
             + (2 * (song_chorus + '\n'))
